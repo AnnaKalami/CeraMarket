77ceeda2c7706049b1ecdcc12572d617e7cbdae6
AnnaKalami <Vampire1621@gmail.com>
2024-02-09 17:36:29 +0300

    compare menu+main

--- client/src/App/App.tsx
+++ client/src/App/App.tsx
@@ -13,11 +13,8 @@ import { loadItems, stopLoading } from '../features/item/ItemsSlice';
 function App(): JSX.Element {
 
 const [menu, setMenu] = useState(false);
-console.log(menu);
 
-
-  // const dispatch = useAppDispatch();
-
+  const dispatch = useAppDispatch();
   // const loadUsers = async (): Promise<void> => {
   //   const res = await fetch('/api/users');
   //   const data: { users: User[] } = (await res.json()) as { users: User[] };
@@ -37,12 +34,12 @@ console.log(menu);
       <Routes>
          <Route path="/" element={<NavBar menu={menu} setMenu={setMenu} />} >
          <Route index element={<MainPage />} />
-          <Route path="/heroes" element={<HeroesListPage />} />
+          {/* <Route path="/heroes" element={<HeroesListPage />} />
           <Route path="/users" element={<UsersListPage />} />
           <Route path="/sign-up" element={<RegistrationPage />} />
           <Route path="/sign-in" element={<AuthorizationPage />} />
-          <Route path="/heroes/:heroId" element={<HeroItemPage />} />
-          <Route path="/memo" element={<Callbek />} /> */}
+          <Route path="/heroes/:heroId" element={<HeroItemPage />} /> */}
+         
           
           <Route path="*" element={<h1>404</h1>} />
         </Route>
--- client/src/features/main/MainPage.tsx
+++ client/src/features/main/MainPage.tsx
@@ -2,13 +2,13 @@ import React from 'react';
 import { useSelector } from 'react-redux';
 import { type RootState } from '../../redux/store';
 import MainSwiper from './MainSwiper';
-import SampleSlider from './MainSwiper8';
+// import SampleSlider from './MainSwiper8';
 import 'swiper/css'
 import 'swiper/css/autoplay'
 const MainPage = (): JSX.Element => {
   const allItems = useSelector((store: RootState) => store.items.items)
   const allImages = [...allItems].map((item)=> item.ItemGallery.ItemImages).flat()
-  console.log(allItems);
+  // console.log(allItems);
   
   
   return (
