7afc9205de634e901d73183b2f272cbd0ffc0fdd
AnnaKalami <Vampire1621@gmail.com>
2024-02-15 18:11:57 +0300

    scss chat & main

--- client/src/features/chats/chatList.tsx
+++ client/src/features/chats/chatList.tsx
@@ -1,47 +1,43 @@
-import './styles/chat.scss';
+import './styles/chatList.scss';
+
 import { useSelector } from 'react-redux';
 import React from 'react';
-import { type RootState } from '../../redux/store';
-// import socket from './socket';
-// import { type userId } from "../auth/types";
-
-
-
-// export const socket = io(URL, {
-//     autoConnect: false
-//   });
+import type { RootState } from '../../redux/store';
 
 function ChatListPage(): JSX.Element {
-    // const userId = useSelector((store: RootState) => store.auth.auth?.id);
-    const user = useSelector((store: RootState) => store.auth.auth);
-    const chats = useSelector((store: RootState) => store.chats.chats);
-    const users = useSelector((store: RootState) => store.users.users);
-    // console.log(chats);
-  
-    
+  const user = useSelector((store: RootState) => store.auth.auth);
+  const chats = useSelector((store: RootState) => store.chats.chats);
+  const users = useSelector((store: RootState) => store.users.users);
+
+  // Фильтруем чаты на основе sender_id или receiver_id
+  const filteredChats = chats.filter(
+    (chat) => chat.sender_id === user?.id || chat.receiver_id === user?.id,
+  );
 
   return (
     <div className="chat-list-container">
-    <div className="chat-list-header">
-      <h2>Чаты</h2>
-      <button type='button'>+ Создать чат</button>
+      <div className="chat-list-header">
+        <h2>Чаты</h2>
+      </div>
+      <ul className="chat-list">
+        {filteredChats.map((chat) => {
+          const interlocutorId = chat.sender_id === user?.id ? chat.receiver_id : chat.sender_id;
+          const interlocutor = users.find((u) => u.id === interlocutorId);
+          return (
+            <li className="chat-item" key={chat.id}>
+              <img className="chat-avatar" src={interlocutor?.img} alt="ava" />
+              <div className="chat-details">
+                <div className="chatListDiv">
+                  <a href={`/chats/${chat.id}`} className="user-link">
+                    {interlocutor?.name}
+                  </a>
+                </div>
+              </div>
+            </li>
+          );
+        })}
+      </ul>
     </div>
-    <ul className="chat-list">
-    {chats.map(chat => {
-                    const interlocutorId = chat.sender_id === user?.id ? chat.receiver_id : chat.sender_id;
-                    const interlocutor = users.find(u => u.id === interlocutorId);
-                    return (
-                        <li className="chat-item" key={chat.id}>
-                            <div className="chat-avatar"/>
-                            <div className="chat-details">
-                                <h3><a href={`/chats/${chat.id}`} className="user-link">{interlocutor?.name}</a></h3>
-                                <p>Последнее сообщение в чате</p>
-                            </div>
-                        </li>
-                    );
-                })}
-    </ul>
-  </div>
   );
 }
 
